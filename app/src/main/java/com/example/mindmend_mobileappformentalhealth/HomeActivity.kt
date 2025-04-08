2e2d9d32ba35a57e736ecb3e2ee29d3ad3022bd2
ShubhamS <thessingh08@gmail.com>
2025-04-08 12:45:37 +0530

    added home page fragments and profile section

--- app/src/main/java/com/example/mindmend_mobileappformentalhealth/HomeActivity.kt
+++ app/src/main/java/com/example/mindmend_mobileappformentalhealth/HomeActivity.kt
@@ -5,16 +5,35 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class HomeActivity : AppCompatActivity() {
+    private lateinit var bottomNavigationView: BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_home)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation)
+
+        // Load default fragment
+        loadFragment(TodayFragment())
+
+        bottomNavigationView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.nav_today -> loadFragment(TodayFragment())
+                R.id.nav_practices -> loadFragment(PracticesFragment())
+                R.id.nav_favourite -> loadFragment(FavouriteFragment())
+                R.id.nav_profile -> loadFragment(ProfileFragment())
+            }
+            true
         }
+    }
+
+    private fun loadFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.nav_host_fragment, fragment)
+            .commit()
     }
 }
